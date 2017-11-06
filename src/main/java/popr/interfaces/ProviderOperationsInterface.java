b2bdc3ff340d48dbcf19a5c6a6e612c549b3ba7f
Eryk <Eryk@192.168.1.5>
2017-11-06 03:42:02 +0100

    Changes on provider interface methods ( param types, return type etc. )

--- src/main/java/popr/interfaces/ProviderOperationsInterface.java
+++ src/main/java/popr/interfaces/ProviderOperationsInterface.java
@@ -5,19 +5,19 @@ import org.springframework.data.domain.Pageable;
 import popr.model.Provider;
 import popr.model.Service;
 import popr.model.ServiceOrder;
+import popr.model.ServiceOrderStatus;
 import popr.model.enums.ServiceOrderStatusDict;
 
 import java.util.List;
 
 public interface ProviderOperationsInterface {
 
-    Page<ServiceOrder> getServiceOrdersByProvider(Provider provider, Pageable pageable);
-
-    Service addService(Provider provider, String description, int price,
-                       int estimatedRealisationTime, String name);
-
-    Service changeServiceStatus(ServiceOrderStatusDict status, String description, ServiceOrder serviceOrder);
-
     List<ServiceOrderStatusDict> getAllServiceOrderStatuses();
+
+	List<ServiceOrder> getServiceOrdersByProviderId(Long providerId);
+
+	Service addService(Long providerId, Service service);
+
+	ServiceOrderStatus changeServiceOrderStatus(ServiceOrderStatus newStatusOrder, Long serviceOrderId);
 
 }
--- src/main/java/popr/service/ProviderOperationsService.java
+++ src/main/java/popr/service/ProviderOperationsService.java
@@ -1,46 +1,50 @@
 package popr.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
 import popr.interfaces.ProviderOperationsInterface;
 import popr.model.Provider;
+import popr.model.Service;
 import popr.model.ServiceOrder;
+import popr.model.ServiceOrderStatus;
 import popr.model.enums.ServiceOrderStatusDict;
-import popr.repository.ProviderRepository;
 import popr.repository.ServiceOrderRepository;
+import popr.repository.ServiceOrderStatusRepository;
 import popr.repository.ServiceRepository;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Service
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Service
 @RequiredArgsConstructor
 public class ProviderOperationsService implements ProviderOperationsInterface {
 
     private final ServiceOrderRepository serviceOrderRepository;
     private final ServiceRepository serviceRepository;
+    private final ServiceOrderStatusRepository serviceOrderStatusRepository;
 
     @Override
-    public Page<popr.model.ServiceOrder> getServiceOrdersByProvider(Provider provider, Pageable pageable) {
-        return serviceOrderRepository.findByService_Provider(provider, pageable);
+    public List<ServiceOrder> getServiceOrdersByProviderId(Long providerId) {
+    	List<Service> services = serviceRepository.findByProviderId(providerId);
+    	List<ServiceOrder> orders = serviceOrderRepository.findByServiceId(services.get(0).getId());
+        return orders;
     }
 
     @Override
-    public popr.model.Service addService(Provider provider, String description, int price, int estimatedRealisationTime, String name) {
-        popr.model.Service service = new popr.model.Service();
-        service.setDescription(description);
-        service.setPrice(price);
-        service.setEstimatedRealisationTime(estimatedRealisationTime);
-        service.setProvider(provider);
-
+    public Service addService(Long providerId, Service service) {
+    	Provider provider = new Provider();
+    	provider.setId(providerId);
+    	service.setProvider(provider);
         return serviceRepository.save(service);
     }
 
     @Override
-    public popr.model.Service changeServiceStatus(ServiceOrderStatusDict status, String description, ServiceOrder serviceOrder) {
-        return null;
+    public ServiceOrderStatus changeServiceOrderStatus(ServiceOrderStatus newStatusOrder, Long serviceOrderId) {
+    	ServiceOrder updatedServiceOrder = new ServiceOrder();
+    	updatedServiceOrder.setId(serviceOrderId);
+    	newStatusOrder.setServiceOrder(updatedServiceOrder);
+    	return serviceOrderStatusRepository.save(newStatusOrder);
     }
 
     @Override
