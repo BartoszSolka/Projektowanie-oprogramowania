764914f4cc731a33513859fe084e4841dcd63c02
RafRuc95 <aauroni2@gamil.com>
2017-11-20 00:26:44 +0100

    Zmiany w widoku admina

--- src/main/java/popr/controller/AdminManager.java
+++ src/main/java/popr/controller/AdminManager.java
@@ -6,7 +6,7 @@ import popr.model.*;
 
 public interface AdminManager {
 
-    Provider addProvider(Provider providerJson);
+    Provider addProvider(Provider provider);
     Admin addAdmin(Admin admin);
     User addUser(User user);
     void changeStatusOfChange(String changeId, String statusId, String description);
